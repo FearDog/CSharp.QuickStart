f007c99b14d769d4f9853d6d49e87accd2374dc1
FearDog <FearDog@bk.ru>
2018-12-18 16:11:16 +0500

    Добавил список фильмов

--- Program.cs
+++ Program.cs
@@ -11,6 +11,27 @@ namespace CSharp.CinemaAdviser
     static void Main(string[] args)
     {
       Console.WriteLine("Здравствуйте, дорогой друг. Это программа поможет подобрать вам фильм для вечернего просмотра");
+      Console.WriteLine("Как вас зовут?");
+      String name = Console.ReadLine(); 
+
+      Console.WriteLine("Здравствуйте, " + name);
+
+      Console.WriteLine("Фильм какого жанры вы хотели бы посмотреть: комедия или драмма?");
+      String genre = Console.ReadLine();
+
+      Console.WriteLine("В таком случае, рекомендую  посмотреть вам следующие фильмы:");
+      if (genre == "комедия")
+      {
+        Console.WriteLine("* Иван Васильевич меняет профессию");
+        Console.WriteLine("* Операцыя \"Ы\" и другие приключения Шурика");
+        Console.WriteLine("* Назад в будущее");
+        Console.WriteLine("* В джазе только девушки");
+      }
+      else
+      {
+        Console.WriteLine("* Зеленая миля");
+        Console.WriteLine("* Побег из Шоушенка");
+      }
 
       Console.ReadLine();
     }
