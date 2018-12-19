4500dfb1866def9253cc4f03dd1cd4f19c72143a
FearDog <FearDog@bk.ru>
2018-12-19 10:38:05 +0500

    Черепахой можно управлять. Появляется еда, которая смещается при поедании

---
+++ TurtleGame/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.SmallBasic.Library;
+
+namespace TurtleGame
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      GraphicsWindow.KeyDown += GraphicsWindow_KeyDown;
+      Turtle.PenUp();
+
+      GraphicsWindow.BrushColor = "Red";
+      var eat = Shapes.AddRectangle(10, 10);
+      int x = 200;
+      int y = 200;
+      Shapes.Move(eat, x, y);
+      
+      while (true)
+      {
+        Turtle.Move(10);
+
+        if (Turtle.X >= x && Turtle.X <= x + 10 && Turtle.Y >= y && Turtle.Y <= y + 10)
+        {
+          x = x + 20;
+          Shapes.Move(eat, x, y);
+        }
+      }
+    }
+
+    private static void GraphicsWindow_KeyDown()
+    {
+      if (GraphicsWindow.LastKey == "Up")
+      {
+        Turtle.Angle = 0;
+      }
+      else if (GraphicsWindow.LastKey == "Right")
+      {
+        Turtle.Angle = 90;
+      }
+      else if (GraphicsWindow.LastKey == "Down")
+      {
+        Turtle.Angle = 180;
+      }
+      else if(GraphicsWindow.LastKey == "Left")
+      {
+        Turtle.Angle = 270;
+      }
+    }
+  }
+}
