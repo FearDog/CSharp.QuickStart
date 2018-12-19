7a1e09e2c948dc5b3ec8f67a28b23b673a65c1a0
FearDog <FearDog@bk.ru>
2018-12-19 08:42:51 +0500

    Добавил жанр Эротика

--- Program.cs
+++ Program.cs
@@ -12,12 +12,12 @@ namespace CSharp.CinemaAdviser
     {
       Console.WriteLine("Здравствуйте, дорогой друг. Это программа поможет подобрать вам фильм для вечернего просмотра");
       Console.WriteLine("Как вас зовут?");
-      String name = Console.ReadLine(); 
+      string name = Console.ReadLine(); 
 
       Console.WriteLine("Здравствуйте, " + name);
 
-      Console.WriteLine("Фильм какого жанры вы хотели бы посмотреть: комедия или драмма?");
-      String genre = Console.ReadLine();
+      Console.WriteLine("Фильм какого жанры вы хотели бы посмотреть: комедия, драмма, фантастика, эротика?");
+      string genre = Console.ReadLine();
 
       Console.WriteLine("В таком случае, рекомендую  посмотреть вам следующие фильмы:");
       if (genre == "комедия")
@@ -26,6 +26,28 @@ namespace CSharp.CinemaAdviser
         Console.WriteLine("* Операцыя \"Ы\" и другие приключения Шурика");
         Console.WriteLine("* Назад в будущее");
         Console.WriteLine("* В джазе только девушки");
+      }
+      else if (genre == "фантастика")
+      {
+        Console.WriteLine("* Люди Икс");
+        Console.WriteLine("* Черепашки ниндзя");
+      }
+      else if (genre == "эротика")
+      {
+        Console.WriteLine("Сколько вам лет?");
+        string yearsString = Console.ReadLine();
+        int years = Int32.Parse(yearsString);
+
+        if (years < 18)
+        {
+          Console.WriteLine("К сожалению этот жанр доступен только для 18+");
+          Console.WriteLine("Приходите через " + (18 - years) + " лет");
+        }
+        else
+        {
+          Console.WriteLine("* Эмануэль");
+          Console.WriteLine("* Нимфоманка");
+        }
       }
       else
       {
